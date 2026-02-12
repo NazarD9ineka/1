0cf6717059bc4e317f4de37f91b58f28f57f2167
NazarD9ineka <deineka-n@class.ztk.org.ua>
2026-02-12 11:00:40 +0200

    Add files via upload

---
+++ Реалізуйте обчислення суми трьох чисел, введених з клавіатури.py
@@ -0,0 +1,10 @@
+# Запитуємо три числа у користувача
+number1 = float(input("Введіть перше число: "))
+number2 = float(input("Введіть друге число: "))
+number3 = float(input("Введіть третє число: "))
+
+# Обчислюємо суму
+total_sum = number1 + number2 + number3
+
+# Виводимо результат
+print(f"Сума введених чисел: {total_sum}")
